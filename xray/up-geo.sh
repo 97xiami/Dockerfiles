5583b328f764d3f72dc81bfb61aea9c2d34f01ef
97xiami <2285623277cz@gmail.com>
2021-10-27 19:10:46 +0800

    Create up-geo.sh

---
+++ xray/up-geo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
+SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
+
+rm -rf /usr/share/xray/geo*.dat
+
+wget -O /usr/share/xray/geoip.dat $IP
+wget -O /usr/share/xray/geosite.dat $SITE
