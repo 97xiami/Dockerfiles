bf2c5b0b982bc8ddd44f60c3a65d4e0d7322c741
97xiami <2285623277cz@gmail.com>
2024-12-12 15:59:20 +0800

    Update install.sh

--- ariang/install.sh
+++ ariang/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #Download Files
-echo "Downloading : AriaNG.zip"
-URL=$(curl -s "https://api.github.com/repos/mayswind/AriaNg/releases/latest" | grep "browser_download_url" | grep -E "AriaNG-[0-9]+\.[0-9]+\.[0-9]+\.zip" | cut -d '"' -f 4)
+echo "Downloading : AriaNg.zip"
+URL=$(curl -s "https://api.github.com/repos/mayswind/AriaNg/releases/latest" | grep "browser_download_url" | grep -E "AriaNg-[0-9]+\.[0-9]+\.[0-9]+\.zip" | cut -d '"' -f 4)
 
 if [ -z "$URL" ]; then
   echo "Failed to find the download."
   exit 1
 fi
 
-curl -L -o AriaNG.ZIP $URL || { echo "Failed to download the file."; exit 1; }
+curl -L -o AriaNg.ZIP $URL || { echo "Failed to download the file."; exit 1; }
 
 echo "Download completed."
-unzip AriaNG.ZIP -d ${PWD}
-rm AriaNG.zip
+unzip AriaNg.ZIP -d ${PWD}
+rm AriaNg.zip
